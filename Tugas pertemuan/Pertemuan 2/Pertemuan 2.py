9debc81dc18fe5d2a28986897a013a768ffaffda
nahdiyailmy947 <nahdiyailmy947@gmail.com>
2026-06-05 23:03:44 +0700

    update tugas pertemuan 2 nahdiya

--- Tugas pertemuan/Pertemuan 2/Pertemuan 2.py
+++ Tugas pertemuan/Pertemuan 2/Pertemuan 2.py
@@ -1,70 +1,77 @@
 # Nomor 1
-print("2")
-print(2)
+print("saya Nahdiya")
+print("lucu")
 
 # Nomor 2
-print("hello world")
-print("ini print lagi \n Tapi ada 2 baris")
+print(11111111)
+print(-11111111)
 
 # Nomor 3
 print(0x147)  # Hexadecimal
 print(0o147)  # Octal
 
 # Nomor 4
-print(0.4, 4.0)
-print(.4, 4.)
-print(4e-1, 0.4e1)
+# float
+print(0.4)
+print(4.0)
+print(.4) 
+print(4.)
+# eksponen
+print(4e-1)
+print(4e1)
 
 # Nomor 5
 print('I\'m master nahdiya. ')
-print('I\'m "master nahdiya" ')
 print("I'm \"master nahdiya\"")
 
 # Nomor 6
-def print_boolean_literals():
-    print(True)
-    print(False)
-print_boolean_literals()
+print(True)
+print(False)
 
 # Nomor 7
-print("5 ** 3 =", 5 ** 3)
-print("4 ** 2.5 =", 4 ** 2.5)
-print("1.5 ** 2 =", 1.5 ** 2)
-print("2.5 ** 1.5 =", 2.5 ** 1.5)
+print(2 ** 3)
+print(2 ** 3.0)
+print(2.0 ** 3)
+print(2.0 ** 3.0)
 
 # Nomor 8
-print("2 * 2 =", 2 * 2)
-print("4 * 0.2 =", 4 * 0.2)
-print("2.0 * 4 =", 2.0 * 4)
-print("1.2 * 0.2 =", 1.2 * 0.2)
+print(2 * 3)
+print(2 * 3.0)
+print(2.0 * 3)
+print(2.0 * 3.0)
 
 # Nomor 9
-print("10 / 2 =", 10 / 2)
-print("5 / 0.5 =", 5 / 0.5)
-print("7.5 / 3 =", 7.5 / 3)
-print("1.2 / 0.3 =", 1.2 / 0.3)
+print(4 / 2)
+print(2 / 2.0)
+print(2.0 / 2)
+print(2.0 / 2.0)
 
 # Nomor 10
-print("20 // 6 =", 20 // 6)
-print("15 // 2.0 =", 15 // 2.0)
-print("9.5 // 2.0 =", 9.5 // 2.0)
-print("10.5 // 2.5 =", 10.5 // 2.5)
+print(7 // 3)      
+print(7 // 3.0)    
+print(7.0 // 3)    
+print(7.0 // 3.0)  
 
 # Nomor 11
-print("27 % 6 =", 27 % 6)
-print("19 % 5.0 =", 19 % 5.0)
-print("9.5 % 2.0 =", 9.5 % 2.0)
-print("1.5 % 4.5 =", 1.5 % 4.5)
+print(2 % 3)
+print(2 % 3.0)
+print(2.0 % 3)
+print(2.0 % 3.0)
 
 # Nomor 12
-print(-2 + 4)
-print(-6 + 3)
-print(-5 - 4)
-print(2. - 8)
-print(-10 * 2)
+# Operator Unary
+print(-2)
+print(+ 4)
+
+# Operator Binary
+print(6 + 3)
+print(5 - 4)
+print(10 * 2)
 
 # Nomor 13
-print(((4*((40 % 18) + 96) / (4* 10))))
+# Subekpresi (Prioritas Operator)
+print((5 + 4) / 3)
+print((15 - 5) * 2)
 
 # Nomor 14 kuis
 print((3 ** 3), (3 * 5.), (3 * 5))
