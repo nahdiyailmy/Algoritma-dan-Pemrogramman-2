d5df88283deac1f9d225e50570b998d613a6f522
Masterni 947 <nahdiyailmy947@gmail.com>
2026-06-06 07:59:55 +0700

    Add content to Pertemuan 2.py

---
+++ Tugas pertemuan/Pertemuan 2/Pertemuan 2.py
@@ -0,0 +1,79 @@
+# Nomor 1
+print("saya Nahdiya")
+print("lucu")
+
+# Nomor 2
+print(11111111)
+print(-11111111)
+
+# Nomor 3
+print(0x147)  # Hexadecimal
+print(0o147)  # Octal
+
+# Nomor 4
+# float
+print(0.4)
+print(4.0)
+print(.4) 
+print(4.)
+# eksponen
+print(4e-1)
+print(4e1)
+
+# Nomor 5
+print('I\'m master nahdiya. ')
+print("I'm \"master nahdiya\"")
+
+# Nomor 6
+print(True)
+print(False)
+
+# Nomor 7
+print(2 ** 3)
+print(2 ** 3.0)
+print(2.0 ** 3)
+print(2.0 ** 3.0)
+
+# Nomor 8
+print(2 * 3)
+print(2 * 3.0)
+print(2.0 * 3)
+print(2.0 * 3.0)
+
+# Nomor 9
+print(4 / 2)
+print(2 / 2.0)
+print(2.0 / 2)
+print(2.0 / 2.0)
+
+# Nomor 10
+print(7 // 3)      
+print(7 // 3.0)    
+print(7.0 // 3)    
+print(7.0 // 3.0)  
+
+# Nomor 11
+print(2 % 3)
+print(2 % 3.0)
+print(2.0 % 3)
+print(2.0 % 3.0)
+
+# Nomor 12
+# Operator Unary
+print(-2)
+print(+ 4)
+
+# Operator Binary
+print(6 + 3)
+print(5 - 4)
+print(10 * 2)
+
+# Nomor 13
+# Subekpresi (Prioritas Operator)
+print((5 + 4) / 3)
+print((15 - 5) * 2)
+
+# Nomor 14 kuis
+print((3 ** 3), (3 * 5.), (3 * 5))
+print((-3 / 2), (3 / 2), (3 // 2), (-3 // 2))
+print((3 % -5), (3 % 5), (2  ** 2 ** 3))
